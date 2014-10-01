990b7f58e013a7fc036a3974caf10156e1ffe9a1
Ran Ziv <ran@gigaspaces.com>
2014-10-01 12:54:23 +0300

    updated simple provider code; updated type and packages in openstack blueprint

--- simple/scripts/configure.py
+++ simple/scripts/configure.py
@@ -13,31 +13,21 @@
 #    * See the License for the specific language governing permissions and
 #    * limitations under the License.
 
-import tempfile
-
-import fabric
-import fabric.api
-
 from cloudify import ctx
+from cloudify_cli.bootstrap.tasks import (
+    PRIVATE_IP_RUNTIME_PROPERTY,
+    PROVIDER_RUNTIME_PROPERTY
+)
 
 
 def configure(private_ip):
 
-    # configure local
-    key_path = fabric.api.env.key_filename
-    ctx.runtime_properties['local_agent_key_path'] = key_path
-
     # set private ip for manager server
-    ctx.runtime_properties['private_ip'] = private_ip
+    ctx.runtime_properties[PRIVATE_IP_RUNTIME_PROPERTY] = private_ip
 
     # set provider context
-    ctx.runtime_properties['provider_context'] = {
-        'provider_prop1': 'provider_value1',
-        'provider_prop2': [1, 2, 3, 34]
+    provider = {
+        'name': 'simple',
+        'context': dict()
     }
-
-    tmp = tempfile.mktemp()
-
-    with open(tmp, 'w') as f:
-        f.write('some content')
-    fabric.api.put(tmp, '/tmp/tmp_file')
+    ctx.runtime_properties[PROVIDER_RUNTIME_PROPERTY] = provider
