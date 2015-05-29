53214a5e31662fc381e0e5e26363129541750981
Anna Szpoton <anna.szpoton@codilime.com>
2015-05-29 18:09:05 +0200

    Add security groups to context provider

--- gcp/scripts/configure.py
+++ gcp/scripts/configure.py
@@ -15,12 +15,28 @@
 import fabric.api
 import os
 
+from cloudify import ctx
+from plugin import utils
+from plugin import gcp.utils
 
-AWS_DEFAULT_CONFIG_PATH = '/home/ubuntu/gcp_config.json'
+
+GCP_DEFAULT_CONFIG_PATH = '/home/ubuntu/gcp_config.json'
 
 
-def configure_manager(manager_config_path=AWS_DEFAULT_CONFIG_PATH,
+def configure_manager(manager_config_path=GCP_DEFAULT_CONFIG_PATH,
                       gcp_config=None):
     auth = gcp_config.get('auth')
     if auth and os.path.isfile(auth):
         fabric.api.put(auth, manager_config_path)
+    node_instances = ctx._endpoint.storage.get_node_instances()
+    resources = {}
+    for node_instance in node_instances:
+        if node_instance.node_id in utils.SECURITY_GROUPS:
+            run_props = node_instance.runtime_properties
+            resources[node_instance.node_id] = {
+                'id': run_props[utils.NAME],
+                gcp.utils.TARGET_TAGS: run_props[gcp.utils.TARGET_TAGS],
+                gcp.utils.SOURCE_TAGS: run_props[gcp.utils.SOURCE_TAGS]}
+    resources[utils.GCP_CONFIG] = manager_config_path
+    provider = {'resources': resources}
+    ctx.instance.runtime_properties['provider_context'] = provider
