5feb6c36c6006026f0de44503a8a74b33331d9b7
Michael Sverdlik <michael@gigaspaces.com>
2015-06-23 18:37:12 +0300

    CFY-2919 modify logrotate config

--- simple/components/logstash/scripts/create.sh
+++ simple/components/logstash/scripts/create.sh
@@ -20,5 +20,20 @@ yum_install ${LOGSTASH_SOURCE_URL}
 logstash_conf=$(ctx download-resource "components/logstash/config/logstash.conf")
 sudo mv ${logstash_conf} "${LOGSTASH_CONF_PATH}/logstash.conf"
 
+lconf="/etc/logrotate.d/logstash"
+config="$LOGSTASH_LOG_PATH/*.log {
+        daily
+        rotate 7
+        copytruncate
+        compress
+        delaycompress
+        missingok
+        notifempty
+}"
+
+ctx logger info "Configuring logrotate..."
+echo "$config" | sudo tee $lconf
+sudo chmod 644 $lconf
+
 # sudo systemctl enable logstash.service
 sudo /sbin/chkconfig logstash on
