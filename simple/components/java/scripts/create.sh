2d1c287efc690327c5c70076c195ab6be15a4cfa
nir0s <nir36g@gmail.com>
2015-06-24 13:21:50 +0300

    fixes

--- simple/components/java/scripts/create.sh
+++ simple/components/java/scripts/create.sh
@@ -18,6 +18,6 @@ copy_notice "java"
 # sudo mkdir ${JAVA_HOME} && sudo tar -xzvf /tmp/java.tar.gz -C ${JAVA_HOME} --strip=1 &&
 
 ctx logger info "Downloading java from: ${JAVA_DOWNLOAD_URL}..."
-curl --fail -L --header "Cookie: oraclelicense=accept-securebackup-cookie" ${JAVA_DOWNLOAD_URL} --create-dirs -o /tmp/java.rpm >/dev/null
+curl --fail -L --silent --header "Cookie: oraclelicense=accept-securebackup-cookie" ${JAVA_DOWNLOAD_URL} --create-dirs -o /tmp/java.rpm >/dev/null
 ctx logger info "Installing Java..."
 sudo rpm -ivh /tmp/java.rpm >/dev/null
