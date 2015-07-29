0eda24aca705d8a6ea7575de05810d4ae07bc24c
nir0s <nir36g@gmail.com>
2015-07-29 20:16:57 +0300

    CFY-3096 removed agent resource downloads from create

--- simple/components/nginx/scripts/create.sh
+++ simple/components/nginx/scripts/create.sh
@@ -6,8 +6,6 @@ CONFIG_REL_PATH="components/nginx/config"
 
 export NGINX_SOURCE_URL=$(ctx node properties nginx_rpm_source_url)  # (e.g. "https://dl.dropboxusercontent.com/u/407576/3.2/nginx-1.8.0-1.el7.ngx.x86_64.rpm")
 export REST_SERVICE_SOURCE_URL=$(ctx node properties rest_service_module_source_url)  # (e.g. "https://github.com/cloudify-cosmo/cloudify-manager/archive/3.2.tar.gz")
-export CENTOS_7_AGENT_SOURCE_URL=$(ctx node properties centos_7_agent_source_url)  # (e.g. "https://dl.dropboxusercontent.com/u/407576/centos-Core-agent.tar.gz")
-export UBUNTU_TRUSTY_AGENT_SOURCE_URL=$(ctx node properties ubuntu_trusty_agent_source_url)
 
 export REQUIRE_TTY_SOURCE_URL="https://raw.githubusercontent.com/cloudify-cosmo/cloudify-packager/CFY-2596-centos7-agent/package-configuration/centos-agent/centos-agent-disable-requiretty.sh"
 export CELERY_CONF_SOURCE_URL="https://raw.githubusercontent.com/cloudify-cosmo/cloudify-packager/CFY-2596-centos7-agent/package-configuration/centos-agent/centos-celeryd-cloudify.conf.template"
@@ -75,19 +73,6 @@ ctx logger info "Extracting Manager Resources to ${MANAGER_RESOURCES_HOME}..."
 tar -xzf ${manager_repo} --strip-components=1 -C "/tmp" >/dev/null
 sudo cp -R "/tmp/resources/rest-service/cloudify/" "${MANAGER_RESOURCES_HOME}"
 
-
-ctx logger info "Downloading Centos Agent resources..."
-# the same mechanism should be used to create a loop for downloading multiple agents
-# from an inputs of a list of agent urls or paths
-centos_7_agent_file=$(download_file ${CENTOS_7_AGENT_SOURCE_URL})
-# agent_file_name=$(get_file_name "${CENTOS_7_AGENT_SOURCE_URL}")
-# sudo mv ${agent_file} "${MANAGER_AGENTS_PATH}/${agent_file_name}"
-sudo mv ${centos_7_agent_file} "${MANAGER_AGENTS_PATH}/centos-Core-agent.tar.gz"
-
-# temporarily explicitly download Ubuntu trusty agent for testing.
-# this will be removed when we download multiple agents
-ubuntu_trusty_agent_file=$(download_file ${UBUNTU_TRUSTY_AGENT_SOURCE_URL})
-sudo mv ${ubuntu_trusty_agent_file} "${MANAGER_AGENTS_PATH}/Ubuntu-trusty-agent.tar.gz"
 
 require_tty_script=$(download_file ${REQUIRE_TTY_SOURCE_URL})
 sudo cp ${require_tty_script} "${MANAGER_SCRIPTS_PATH}/centos-agent-disable-requiretty.sh"
