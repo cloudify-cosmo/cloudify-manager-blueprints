17424a2993199d6c96e3c7fa1d444f8c8ed8d294
nir0s <nir36g@gmail.com>
2015-08-04 06:55:33 +0300

    CFY-2875 requiretty fix

--- simple/components/manager/scripts/configure_manager.sh
+++ simple/components/manager/scripts/configure_manager.sh
@@ -27,6 +27,7 @@ function _disable_requiretty() {
         # for now, this will have to do. yuch.
         # if we decide to change this:
         # http://0pointer.de/blog/projects/os-release.html
+        # http://unix.stackexchange.com/questions/6345/how-can-i-get-distribution-name-and-version-number-in-a-simple-shell-script
         ###
         if grep -i 'centos' /proc/version > /dev/null; then
             echo 'supported'
@@ -46,12 +47,15 @@ function _disable_requiretty() {
         # Disables requiretty for a specific user ${whoami}.
         # by creating a user specific /etc/sudoers.d file and applying the directive for it.
         ###
+        whoami=$(whoami)
+        requiretty='!requiretty'
+
         if sudo grep -q -E '[^!]requiretty' /etc/sudoers; then
-            echo "Creating sudoers user file and setting disable requiretty directive."
+            ctx logger info "Creating sudoers user file and setting disable requiretty directive."
             echo "Defaults:${whoami} ${requiretty}" | sudo tee /etc/sudoers.d/${whoami} >/dev/null
             sudo chmod 0440 /etc/sudoers.d/${whoami}
         else
-            echo "No requiretty directive found, nothing to do."
+            ctx logger info "No requiretty directive found, nothing to do."
         fi
     }
 
@@ -62,21 +66,17 @@ function _disable_requiretty() {
         if [ ! -f "/etc/sudoers" ]; then
             error_exit 116 "Could not find sudoers file at expected location (/etc/sudoers)"
         fi
-        echo "Setting directive in /etc/sudoers."
+        ctx logger info "Setting directive in /etc/sudoers."
         sudo sed -i 's/^Defaults.*requiretty/#&/g' /etc/sudoers || error_exit_on_level $? 117 "Failed to edit sudoers file to disable requiretty directive" 1
     }
-
-    # http://unix.stackexchange.com/questions/6345/how-can-i-get-distribution-name-and-version-number-in-a-simple-shell-script
-    whoami=$(whoami)
-    requiretty='!requiretty'
 
     # for supported distros, this will disable requiretty for a specific user.
     # Otherwise, it will disable it for all users.
     if [ "$(get_distro)" != 'unsupported' ]; then
-        echo "Distro is supported."
+        ctx logger info "Distro is supported."
         disable_for_user
     else
-        echo "Distro is unsupported."
+        ctx logger info "Distro is unsupported."
         disable_for_all_users
     fi
 }
@@ -86,4 +86,4 @@ function _disable_requiretty() {
 # sudo sed -i '0,/^#Defaults.*requiretty/s//Defaults requiretty/' "/etc/sudoers"
 
 _post_provider_context
-_disable_requirettys
+_disable_requiretty
