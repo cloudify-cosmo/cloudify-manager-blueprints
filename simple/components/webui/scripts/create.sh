2aa6d5539f5cb357e90a2c98c2324b87ee4d74b8
Eden <eden@gigaspaces.com>
2015-08-13 12:10:44 +0300

    feat(UI+Kibana Blueprint)
    updated manager centos 7 simple blueprint to run with kibana and kibana-proxy after bootstraping and nginx to proxy the requests
    
    CFY-2980

--- simple/components/webui/scripts/create.sh
+++ simple/components/webui/scripts/create.sh
@@ -8,11 +8,13 @@ CONFIG_REL_PATH="components/webui/config"
 export NODEJS_SOURCE_URL=$(ctx node properties nodejs_tar_source_url)  # (e.g. "http://nodejs.org/dist/v0.10.35/node-v0.10.35-linux-x64.tar.gz")
 export WEBUI_SOURCE_URL=$(ctx node properties webui_tar_source_url)  # (e.g. "https://dl.dropboxusercontent.com/u/407576/cosmo-ui-3.2.0-m4.tgz")
 export GRAFANA_SOURCE_URL=$(ctx node properties grafana_tar_source_url)  # (e.g. "https://dl.dropboxusercontent.com/u/407576/grafana-1.9.0.tgz")
+export KIBANA_SOURCE_URL=$(ctx node properties kibana_tar_source_url)  # (e.g. "https://dl.dropboxusercontent.com/u/407576/kibana-4.1.1.tgz")
 
 export NODEJS_HOME="/opt/nodejs"
 export WEBUI_HOME="/opt/cloudify-ui"
 export WEBUI_LOG_PATH="/var/log/cloudify/webui"
 export GRAFANA_HOME="${WEBUI_HOME}/grafana"
+export KIBANA_HOME="${WEBUI_HOME}/kibana"
 
 
 ctx logger info "Installing Cloudify's WebUI..."
@@ -26,6 +28,7 @@ create_dir ${WEBUI_HOME}
 create_dir ${WEBUI_HOME}/backend
 create_dir ${WEBUI_LOG_PATH}
 create_dir ${GRAFANA_HOME}
+create_dir ${KIBANA_HOME}
 
 ctx logger info "Installing NodeJS..."
 nodejs=$(download_file ${NODEJS_SOURCE_URL})
@@ -40,6 +43,10 @@ sudo tar -xzvf ${webui} -C ${WEBUI_HOME} --strip-components=1 >/dev/null
 ctx logger info "Installing Grafana..."
 grafana=$(download_file ${GRAFANA_SOURCE_URL})
 sudo tar -xzvf ${grafana} -C ${GRAFANA_HOME} --strip-components=1 >/dev/null
+
+ctx logger info "Installing Kibana..."
+kibana=$(download_file ${KIBANA_SOURCE_URL})
+sudo tar -xzvf ${kibana} -C ${KIBANA_HOME} --strip-components=1 >/dev/null
 
 ctx logger info "Deploying WebUI Configuration..."
 deploy_file "${CONFIG_REL_PATH}/gsPresets.json" "${WEBUI_HOME}/backend/gsPresets.json"
--- simple/components/webui/scripts/start.sh
+++ simple/components/webui/scripts/start.sh
@@ -2,3 +2,6 @@
 
 ctx logger info "Starting WebUI Backend..."
 sudo systemctl start cloudify-webui.service
+
+ctx logger info "Starting Kibana..."
+sudo bash /opt/cloudify-ui/kibana/run.sh
