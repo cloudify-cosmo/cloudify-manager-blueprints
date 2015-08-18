46be5dc8d0c359f1ac2b841d51b1bddaff3ca3cb
nir0s <nir36g@gmail.com>
2015-08-18 15:10:00 +0300

    CFY-2875 cleanup plus moved the blueprint to a new location before merge

--- simple/components/bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# source ~/cfy/bin/activate
-# cfy local init --blueprint-path simple-manager-blueprint.yaml --inputs inputs.yaml --install-plugins
-# cfy local execute -w install -v
-
-cfy init -r
-cfy bootstrap --install-plugins -p simple-manager-blueprint.yaml -i inputs.yaml --keep-up-on-failure -v
--- simple/components/provision_host.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo '
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCjM5GpakaKCpVJgt4biaTH4CP76qlJc0L5JkRFnhS9T7A9jYBFEaFgQwlw+cSZ0sq/KHa8kXwDRlUiRkSghA0MSs8r4JhPMhbpyiLzs20hCy6ahVFVLzgzjDOQsVODOZJ2JLBiyxPp6wJwBn2jdoKUl9dCrqm8h+6tAPP9naJXOwQCE+jSW6HyZhf9z8Z1kUlb4bZNHWwRR2AjUPip/ZZHHmEvr1xFZyOLK9g8X3UGibnS59oKqAMO5X5vXZYHWh923If0vBV31zessUPM07eTx/sYU4NVAc8jCDC/EZ/LR+KBTox25kh++8NTocWuosI2n0U/Kw9HsrZiixlQmLsd nir0s@nir0s-x1
-' >> ~/.ssh/test_public_key.pub
-cat ~/.ssh/test_public_key.pub >> ~/.ssh/authorized_keys
