68c35ab617c75fb4af70b993f4eb4209f5b1ea1a
Michael Sverdlik <mhsver@gmail.com>
2015-12-01 15:23:58 +0200

    CFY-4313 add auto restart to nginx

--- components/nginx/scripts/create.sh
+++ components/nginx/scripts/create.sh
@@ -12,6 +12,7 @@ export MANAGER_RESOURCES_HOME="/opt/manager/resources"
 export MANAGER_AGENTS_PATH="${MANAGER_RESOURCES_HOME}/packages/agents"
 export MANAGER_SCRIPTS_PATH="${MANAGER_RESOURCES_HOME}/packages/scripts"
 export MANAGER_TEMPLATES_PATH="${MANAGER_RESOURCES_HOME}/packages/templates"
+NGINX_UNIT_OVERRIDE="/etc/systemd/system/nginx.service.d"
 
 # this is propagated to the agent retrieval script later on so that it's not defined twice.
 ctx instance runtime_properties agent_packages_path "${MANAGER_AGENTS_PATH}"
@@ -31,7 +32,12 @@ create_dir ${MANAGER_AGENTS_PATH}
 create_dir ${MANAGER_SCRIPTS_PATH}
 create_dir ${MANAGER_TEMPLATES_PATH}
 
+create_dir ${NGINX_UNIT_OVERRIDE}
+
 yum_install ${NGINX_SOURCE_URL}
+
+ctx logger info "Creating systemd unit override..."
+deploy_blueprint_resource "${CONFIG_REL_PATH}/restart.conf" "${NGINX_UNIT_OVERRIDE}/restart.conf"
 
 deploy_logrotate_config "nginx"
 
