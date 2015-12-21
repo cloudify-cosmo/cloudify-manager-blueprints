0404aedfa81818b456b456d946ad601409b4184f
Denis Pauk <pauk.denis@gmail.com>
2015-12-21 17:01:46 +0200

    CFY-4501: use sudo for not root admins

--- components/manager/scripts/vcloud/update-vcloud-vm.sh
+++ components/manager/scripts/vcloud/update-vcloud-vm.sh
@@ -6,19 +6,19 @@ uname -a
 # yum update --skip-broken -y
 
 # install requirements for python-lxml
-yum install python-devel gcc-c++ gcc libxslt-devel libxml2-devel python-argparse zlib-devel -y
+sudo yum install python-devel gcc-c++ gcc libxslt-devel libxml2-devel python-argparse zlib-devel -y
 
 # add http(s) rules
-firewall-cmd --zone=public --add-port=80/tcp --permanent
-firewall-cmd --zone=public --add-port=443/tcp --permanent
+sudo firewall-cmd --zone=public --add-port=80/tcp --permanent
+sudo firewall-cmd --zone=public --add-port=443/tcp --permanent
 # add influxdb connection
-firewall-cmd --zone=public --add-port=8086/tcp --permanent
+sudo firewall-cmd --zone=public --add-port=8086/tcp --permanent
 # port for agent download
-firewall-cmd --zone=public --add-port=53229/tcp --permanent
+sudo firewall-cmd --zone=public --add-port=53229/tcp --permanent
 # port for AQMP
-firewall-cmd --zone=public --add-port=5672/tcp --permanent
+sudo firewall-cmd --zone=public --add-port=5672/tcp --permanent
 # port for diamond
-firewall-cmd --zone=public --add-port=8101/tcp --permanent
-firewall-cmd --zone=public --add-port=8100/tcp --permanent
+sudo firewall-cmd --zone=public --add-port=8101/tcp --permanent
+sudo firewall-cmd --zone=public --add-port=8100/tcp --permanent
 
-firewall-cmd --reload
+sudo firewall-cmd --reload
