0ed6f6ccbd315584ed55b9dc1c435504851860e8
Łukasz Maksymczuk <lukasz@lukasz-maksymczuk.intra.codilime.com>
2016-05-12 12:32:24 +0200

    CFY-4953 Pre-upgrade validations

--- components/nginx/scripts/start.py
+++ components/nginx/scripts/start.py
@@ -25,6 +25,5 @@ def check_response(response):
 utils.start_service(NGINX_SERVICE_NAME, append_prefix=False)
 utils.systemd.verify_alive(NGINX_SERVICE_NAME, append_prefix=False)
 
-nginx_url = '{0}://127.0.0.1/api/v2.1/blueprints'.format(
-    ctx.instance.runtime_properties['rest_protocol'])
+nginx_url = 'http://127.0.0.1/api/v2.1/blueprints'
 utils.verify_service_http(NGINX_SERVICE_NAME, nginx_url, check_response)
