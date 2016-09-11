893eb5ade6c1d2dca61143ef0b5594901fd8b460
Omer Duskin <omerd@gigaspaces.com>
2016-09-11 17:11:12 +0300

    CFY-5802 updat configs related to pg

--- components/postgresql/config/create_default_db.sh
+++ components/postgresql/config/create_default_db.sh
@@ -37,6 +37,7 @@ function create_admin_user() {
     run_psql "CREATE USER $user WITH PASSWORD '$password';"
     run_psql "GRANT ALL PRIVILEGES ON DATABASE $db TO $user;"
     run_psql "ALTER USER $user CREATEDB;"
+    run_psql "ALTER DATABASE $db OWNER TO $user;"
 }
 
 clean_database_and_user ${db_name} ${user}
