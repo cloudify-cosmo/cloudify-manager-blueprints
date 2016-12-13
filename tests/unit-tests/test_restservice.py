134135d3bf71c411b587f508db009f493556b617
Idan Moyal <idanmo@gmail.com>
2016-12-13 07:05:36 +0000

    CFY-6161 make number of gunicorn workers configurable and limited to 12 (#444)

---
+++ tests/unit-tests/test_restservice.py
@@ -0,0 +1,50 @@
+import os
+
+from jinja2 import (
+    Environment,
+    FileSystemLoader,
+)
+from testtools import TestCase
+from testtools.matchers import Contains
+
+
+class TestRestServiceFile(TestCase):
+
+    """Test service file."""
+
+    TEMPLATE_DIR = os.path.join(
+        os.path.dirname(__file__),
+        os.pardir,
+        os.pardir,
+        'components',
+        'restservice',
+        'config',
+    )
+
+    @classmethod
+    def setUpClass(cls):
+        """Get template object."""
+        env = Environment(loader=FileSystemLoader(cls.TEMPLATE_DIR))
+        cls.template = env.get_template('cloudify-restservice.service')
+
+    def test_render_default(self):
+        """Render template using 0 as the worker count."""
+        text = self.template.render(node={
+            'properties': {
+                'gunicorn_worker_count': 0,
+            },
+        })
+        self.assertThat(text, Contains('-w $((${WORKER_COUNT} > ${MAX_WORKER_COUNT} ? ${MAX_WORKER_COUNT} : ${WORKER_COUNT})) \\'))  # NOQA
+
+    def test_render_integer(self):
+        """Render template using an integer as the worker count."""
+        expected_worker_count = 12345
+        text = self.template.render(node={
+            'properties': {
+                'gunicorn_worker_count': expected_worker_count,
+            },
+        })
+        self.assertThat(
+            text,
+            Contains('-w {0} \\'.format(expected_worker_count)),
+        )
