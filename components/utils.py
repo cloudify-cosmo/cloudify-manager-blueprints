2a4bf64e62865efcf0d1606885225b0d59826a46
Anna Szpoton <anna.szpoton@gigaspaces.com>
2017-06-08 22:03:17 +0200

    Revert previous ssl cert check if exists

--- components/utils.py
+++ components/utils.py
@@ -364,8 +364,7 @@ def deploy_or_generate_external_ssl_cert(ips, cn):
         EXTERNAL_SSL_KEY_FILENAME
     )
 
-    if os.path.isfile(user_provided_cert_path) and \
-            os.path.isfile(user_provided_key_path):
+    try:
         # Try to deploy user provided certificates
         deploy_blueprint_resource(user_provided_cert_path,
                                   cert_target_path,
@@ -385,21 +384,24 @@ def deploy_or_generate_external_ssl_cert(ips, cn):
             )
         )
         return cert_target_path, key_target_path
-    else:
-        ctx.logger.info(
-            'Generating SSL certificate `{0}` and SSL private '
-            'key `{1}`'.format(
-                EXTERNAL_SSL_CERT_FILENAME,
-                EXTERNAL_SSL_KEY_FILENAME
+    except Exception as e:
+        if "No such file or directory" in e.stderr:
+            ctx.logger.info(
+                'Generating SSL certificate `{0}` and SSL private '
+                'key `{1}`'.format(
+                    EXTERNAL_SSL_CERT_FILENAME,
+                    EXTERNAL_SSL_KEY_FILENAME
+                )
             )
-        )
 
-        return _generate_ssl_certificate(
-            ips,
-            cn,
-            EXTERNAL_SSL_CERT_FILENAME,
-            EXTERNAL_SSL_KEY_FILENAME,
-        )
+            return _generate_ssl_certificate(
+                ips,
+                cn,
+                EXTERNAL_SSL_CERT_FILENAME,
+                EXTERNAL_SSL_KEY_FILENAME,
+            )
+        else:
+            raise
 
 
 def write_to_tempfile(contents):
