031b9822e53a99fc079edba80fe1dcdd03ef0b33
Lukasz Maksymczuk <tehasdf@users.noreply.github.com>
2017-08-31 13:33:09 +0200

    CFY-7215 Use storage_manager directly in ipsetter (#635)
    * Direct db implementation of update-provider-context.py
    
    It's much faster to use the storage_manager, than to go through the
    API - and we don't need restservice to be running, which creates
    a dependency cycle
    (restservice needs to be up to run ipsetter, but without ipsetter
    the restservice runs under an old cert)
    
    * ipsetter: run the update ctx script from the restservice env
    
    * ipsetter can now run before other services
    
    Restarting them isn't required then
    
    * Use the restservice env vars for ipsetter
    
    We're now using storage_manager directly so we need to have access
    to the restservice config file
    
    * Run ipsetter as root
    
    It's useless to run it as cfyuser and then immediately run sudo
    
    * Use setup_flask_app in update_provider_context
    
    Defaults used in there don't require the config file

--- components/manager-ip-setter/scripts/update-provider-context.py
+++ components/manager-ip-setter/scripts/update-provider-context.py
@@ -1,56 +1,17 @@
-#!/bin/python
-
-import json
 import sys
-import time
-
-import requests
+from manager_rest.flask_utils import setup_flask_app
+from manager_rest.storage import get_storage_manager, models
+from manager_rest.constants import PROVIDER_CONTEXT_ID
+from sqlalchemy.orm.attributes import flag_modified
 
 
 def update_provider_context(manager_ip):
-    username = "{{ ctx.node.properties.admin_username }}"
-    password = "{{ ctx.node.properties.admin_password }}"
-    auth = (username, password)
-    headers = {"Tenant": "default_tenant", 'Content-Type': 'application/json'}
-    print('- Getting provider context...')
-    attempt = 1
-    while True:
-        try:
-            r = requests.get(
-                'http://localhost/api/version', auth=auth, headers=headers)
-            if r.status_code == 200:
-                print('- REST API is up!')
-                break
-            if attempt == 10:
-                break
-        except Exception as e:
-            print('- Error accessing REST API: {}'.format(e))
-        print('- REST API not yet up.. retrying in 5 seconds..')
-        time.sleep(5)
-        attempt += 1
-
-    r = requests.get(
-        'http://localhost/api/v3.1/provider/context',
-        auth=auth, headers=headers)
-    if r.status_code != 200:
-        print("Failed getting provider context.")
-        print(r.text)
-        sys.exit(1)
-    response = r.json()
-    name = response['name']
-    context = response['context']
-    context['cloudify']['cloudify_agent']['broker_ip'] = manager_ip
-    print('- Updating provider context...')
-    data = {'name': name, 'context': context}
-    r = requests.post(
-        'http://localhost/api/v3.1/provider/context',
-        auth=auth, headers=headers,
-        params={'update': 'true'},
-        data=json.dumps(data))
-    if r.status_code != 200:
-        print("Failed updating provider context.")
-        print(r.text)
-        sys.exit(1)
+    with setup_flask_app().app_context():
+        sm = get_storage_manager()
+        ctx = sm.get(models.ProviderContext, PROVIDER_CONTEXT_ID)
+        ctx.context['cloudify']['cloudify_agent']['broker_ip'] = manager_ip
+        flag_modified(ctx, 'context')
+        sm.update(ctx)
 
 
 if __name__ == '__main__':
