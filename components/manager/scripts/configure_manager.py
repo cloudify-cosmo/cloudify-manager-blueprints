c4ded2849fa51cc0f2205b47f5a5bd3c0e9c6215
Łukasz Maksymczuk <lukasz@lukasz-maksymczuk.intra.codilime.com>
2017-09-05 02:11:19 +0200

    Create the certs in configure_manager
    The certs are used for several things (nginx, rabbitmq, and sending
    them to the agents). It doesn't quite make sense to create them in
    nginx's scripts. Instead, let's create the certs in the
    manager_configuration node, which is really the earliest they can be
    created at.
    
    If, in the future, we decide to use one cert for nginx, and another
    for rabbitmq, each service can create its own cert, but the CA would
    still be manager-wide, so should then still be created here, on the
    manager level.

--- components/manager/scripts/configure_manager.py
+++ components/manager/scripts/configure_manager.py
@@ -52,4 +52,14 @@ def configure_security_properties():
     runtime_props['external_rest_protocol'] = external_rest_protocol
 
 
+def create_certs():
+    utils.mkdir(utils.SSL_CERTS_TARGET_DIR)
+    utils.generate_ca_cert()
+    networks = ctx_properties['cloudify']['cloudify_agent']['networks']
+    internal_rest_host = ctx.instance.runtime_properties['internal_rest_host']
+    cert_ips = [internal_rest_host] + list(networks.values())
+    utils.generate_internal_ssl_cert(ips=cert_ips, name=internal_rest_host)
+
+
 configure_security_properties()
+create_certs()
