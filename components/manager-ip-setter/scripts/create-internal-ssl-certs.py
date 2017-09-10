93b5ea276e4b200f901629b64d183c06cfd60472
Łukasz Maksymczuk <lukasz@lukasz-maksymczuk.intra.codilime.com>
2017-09-10 15:42:01 +0200

    CFY-7200 IPSetter: update the default network in cert metadata
    When the IP changes, we update the default network with the new ip
    in the provider context. we also need to update the cert metadata
    file as well.

--- components/manager-ip-setter/scripts/create-internal-ssl-certs.py
+++ components/manager-ip-setter/scripts/create-internal-ssl-certs.py
@@ -24,6 +24,7 @@ if __name__ == '__main__':
         internal_rest_host = cert_metadata['internal_rest_host']
 
     networks = cert_metadata.get('networks', {})
+    networks['default'] = internal_rest_host
     cert_ips = [internal_rest_host] + list(networks.values())
     utils.generate_internal_ssl_cert(ips=cert_ips, name=internal_rest_host)
-    utils.store_cert_metadata(internal_rest_host)
+    utils.store_cert_metadata(internal_rest_host, networks)
