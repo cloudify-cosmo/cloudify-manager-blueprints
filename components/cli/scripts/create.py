426b5fde7eb8378052c93e21c71fe17298ce95f0
limor-gs <limor@cloudify.co>
2017-10-23 14:39:48 +0300

    CFY-7404 - Adding cli installation to bootstrap

--- components/cli/scripts/create.py
+++ components/cli/scripts/create.py
@@ -1,19 +1,17 @@
-from os.path import join, dirname
+import os
 
 from cloudify import ctx
 
-ctx.download_resource(
-    join('components', 'utils.py'),
-    join(dirname(__file__), 'utils.py'))
-import utils # NOQA
+ctx.download_resource(os.path.join('components', 'utils.py'), os.path.join(os.path.dirname(__file__), 'utils.py'))  # NOQA
+import utils  # NOQA
 
 ctx_properties = ctx.node.properties.get_all()
 
 
 def install():
     ctx.logger.info('Installing Cloudify CLI...')
-    source_url = ctx_properties['cli_rpm_url']
-    cli_source_url = ctx_properties.get('cli_rpm_url')
+    source_url = ctx_properties['cli_rpm_source_url']
+    cli_source_url = ctx_properties.get('cli_rpm_source_url')
     ctx.logger.info('source_url={0}'.format(source_url))
     ctx.logger.info('cli_source_url={0}'.format(cli_source_url))
     utils.yum_install(source_url)
