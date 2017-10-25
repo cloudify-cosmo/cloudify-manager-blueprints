80c91cf8be55a561ad5a3b92c931b2b11ba0bafe
limor-gs <limor@cloudify.co>
2017-10-25 17:49:45 +0300

    CFY-7404 - Adding cli installation to bootstrap

--- components/cli/scripts/start.sh
+++ components/cli/scripts/start.sh
@@ -6,5 +6,3 @@ manager_password="$2"
 echo "Using localhost as Manager..."
 
 cfy profiles use localhost -u $manager_username -p $manager_password -t default_tenant
-
-sudo -u root cfy profiles use localhost -u $manager_username -p $manager_password -t default_tenant
